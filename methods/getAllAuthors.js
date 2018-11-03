0e97f6d721d07d88a906d33d4fb706e7d1fed5f4
David Cámara Iglesias <david.c.iglesias@gmail.com>
2018-11-03 17:18:49 +0100

    Minor fix

--- methods/getAllAuthors.js
+++ methods/getAllAuthors.js
@@ -2,25 +2,25 @@ module.exports = async (database) => {
     let sql =  `SELECT idauthor, fullname
                 FROM author`
     var result = {}
-    console.log(`Attempting getAllPosts`)
+    console.log(`Attempting getAllAuthors`)
     await database.query(sql)
         .then((rows) => {
             if(rows.length > 0) {
                 console.log(rows)
                 result.content = rows
                 result.correct = true
-                console.log(`Success in getAllPosts`)
+                console.log(`Success in getAllAuthors`)
                 database.close()
             } else {
                 database.close()
-                console.log(`Error in getAllPosts - No results`)
+                console.log(`Error in getAllAuthors - No results`)
                 result.correct = false
                 result.error = 'No results'
             }
         })
         .catch((err) => {
             database.close()
-            console.log(`Error in getAllPosts - DB Issue ${err}`)
+            console.log(`Error in getAllAuthors - DB Issue ${err}`)
             result.correct = false
             result.error = err
         })
