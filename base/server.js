56a00f819441cfceff1991851b8396d0180777d1
David Cámara Iglesias <david.c.iglesias@gmail.com>
2018-11-11 16:08:19 +0100

    Initial Google login functionality

--- base/server.js
+++ base/server.js
@@ -1,170 +1,228 @@
+require('dotenv').config({ path: './config/data.env', debug: process.env.DEBUG})
+
 const http = require('http')
 const url = require('url')
 const dbSettings = require('./../config/dbSettings')
 const database = require('./../classes/database')
-const port = 2005
+const port = 8080
 const getPostById = require('./../methods/getPostById')
 const getPostByPartialTitle = require('./../methods/getPostByPartialTitle')
 const getAllPosts = require('./../methods/getAllPosts')
 const getAllAuthors = require('./../methods/getAllAuthors')
 const postNewPost = require('./../methods/postNewPost')
 const putUpdateStatus = require('./../methods/putUpdateStatus')
+const passport = require('passport')
+const socketio = require('socket.io')
+const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth')
+const { Strategy: TwitterStrategy } = require('passport-twitter')
+const express = require('express')
+const session = require('express-session')
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
-const example = require('dotenv').config({ path: './config/data.env', debug: process.env.DEBUG})
-//const result = example.parse(Buffer.from('POTATO=potato'))
-console.log(example)
-//console.log(result)
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+app.use(express.json())
+app.use(passport.initialize())
+app.use(cors())
+app.use(session({ 
+    secret: 'KeyboardKittens', 
+    resave: true, 
+    saveUninitialized: true 
+  }))
+
+const TWITTER_CONFIG = {
+    consumerKey: process.env.TWITTER_ACCESS_TOKEN,
+    consumerSecret: process.env.TWITTER_ACCESS_SECRET,
+    callbackURL: 'http://localhost:8080/twitter/callback',
+}
+
+const GOOGLE_CONFIG = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: 'http://localhost:8080/google/callback',
+    scope: 'https://www.googleapis.com/auth/userinfo.profile'
+}
+
+passport.serializeUser((user, cb) => cb(null, user))
+passport.deserializeUser((obj, cb) => cb(null, obj))
+passport.use(new GoogleStrategy(
+    GOOGLE_CONFIG, 
+    (accessToken, refreshToken, profile, cb) => {
+      
+      // save the user right here to a database if you want
+    //   const user = { 
+    //       name: profile.username,
+    //       photo: profile.photos[0].value.replace(/_normal/, '')
+    //   }
+        const user = {
+            name: profile.displayName,
+            photo: profile.photos[0].value.replace(/sz=50/gi, 'sz=250')
+        }
+      cb(null, user)
+    })
+  )
+// passport.use(new TwitterStrategy(
+//     TWITTER_CONFIG, 
+//     (accessToken, refreshToken, profile, cb) => {
+      
+//       // save the user right here to a database if you want
+//       console.log(profile)
+//       const user = { 
+//           name: profile.username,
+//           photo: profile.photos[0].value.replace(/_normal/, '')
+//       }
+//       cb(null, user)
+//     })
+//   )
 
 
+const twitterAuth = passport.authenticate('twitter')
 
-http.createServer(async (request, response) => {
-    response.setHeader('Access-Control-Allow-Origin', '*')
-    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+const googleAuth = passport.authenticate('google', { failureRedirect: '/', scope: ['profile'] })
 
-    request.on('error', (err) => {
-        console.error(err)
-    });
-    
-    response.on('error', (err) => {
-        console.error(err)
-    });
+const addSocketIdToSession = (req, res, next) => {
+    console.log("Adding SocketId to Session")
+    console.log(req.query)
+    req.session.socketId = req.query.socketId
+    next()
+}
 
-    
+app.use(bodyParser.json())
+
+app.get('/twitter', addSocketIdToSession, twitterAuth)
+app.get('/twitter/callback', (req, res, n2) => {console.log("Twitter Back"); n2() },  twitterAuth, (req, res) => {
+    console.log("Returning!")
+    io.in(req.session.socketId).emit('user', req.user)
+    res.end()
+  })
+
+app.get('/google', addSocketIdToSession, googleAuth)
+app.get('/google/callback', (req, res, n2) => {console.log("Google Back"); n2() },  googleAuth, (req, res) => {
+    console.log("Returning!")
+    io.in(req.session.socketId).emit('user', req.user)
+    res.end()
+  })
+
+app.get('/logout', (req, res) => {
+    console.log("logging out!")
+    req.logout();
+    req.session = null;
+    passport
+    res.redirect('/');
+});
+
+app.post('/postNewPost', async(request, response) => {
+    var postRequestJson = request.body
+    const dbConnection = new database(dbSettings)
+    var result = await postNewPost(dbConnection, postRequestJson.idauthor,
+        postRequestJson.title, postRequestJson.content)
+
+    if(!result.correct) {
+        response.status(400).end(result.error.toString())
+    } else {
+        response.set({
+            'Content-Type': 'text/plain',
+            'Charset': 'utf-8',
+            'X-Content-Type-Options': 'nosniff',
+            'Access-Control-Allow-Origin': '*'
+        }).status(201).end()
+    }
+})
+
+app.put('/putUpdate', async(request, response) => {
     const parsedUrl = url.parse(request.url, true)
-    const pathName = parsedUrl.pathname
     const query = parsedUrl.query
     const columnOptions = ['happy', 'sad', 'angry', 'surprised']
 
-    if (pathName && pathName.split("/").pop() === 'favicon.ico') {
-        response.statusCode = 204
-        response.end()
-    }
-    else {
-        if(request.method === "POST"
-        && pathName === '/postNewPost' ) {
-
-            var body = ''
-            request.on('data', async function (data) {
-                body += data.toString();
-            });
-            request.on('end', async function () {
-                try {
-                    var postRequestJson = JSON.parse(body)
-                    const dbConnection = new database(dbSettings)
-                    var result = await postNewPost(dbConnection, postRequestJson.idauthor,
-                        postRequestJson.title, postRequestJson.content)
-                
-                    if(!result.correct) {
-                        response.statusCode = 400
-                        response.end(result.error.toString())
-                    } else {
-                        response.setHeader('Content-Type', 'text/plain; charset=utf-8')
-                        response.setHeader('X-Content-Type-Options', 'nosniff')
-                        response.setHeader('Access-Control-Allow-Origin', '*')
-                        response.statusCode = 201
-                        response.end()
-                    }
-                }
-                catch(err) {
-                    console.log(err)
-                    response.statusCode = 400
-                    response.end(err.toString())
-                }
-            })
-        } 
-        else if(request.method === "PUT"
-        && pathName === '/putUpdate' 
-        && query.id !== undefined
-        && query.column !== undefined && columnOptions.includes(query.column)
-        && query.change !== undefined && (query.change == 1 || query.change == -1)) {
-            console.log('Starting put')
-            const dbConnection = new database(dbSettings)
-            var result = await putUpdateStatus(dbConnection, query.id,
-                query.column, query.change)
-        
-            if(!result.correct) {
-                response.statusCode = 404
-                response.end(result.error.toString())
-            } else {
-                response.statusCode = 200
-                response.end()
-            }
-        } 
-        else if(request.method === "GET"
-        && pathName === '/getPostById' 
-        && query.id !== undefined) {
-            const dbConnection = new database(dbSettings)
-            var result = await getPostById(dbConnection, query.id)
-            
-            if(!result.correct) {
-                response.statusCode = 404
-                response.end(JSON.stringify(result.content))
-            } else {
-                response.setHeader('Content-Type', 'text/plain; charset=utf-8')
-                response.setHeader('X-Content-Type-Options', 'nosniff')
-                response.setHeader('Access-Control-Allow-Origin', '*')
-                response.statusCode = 200
-                response.write(JSON.stringify(result.content))
-                response.end()
-            }
-        }
-        else if(request.method === "GET"
-        && pathName === '/getPostByPartialTitle' 
-        && query.partialTitle !== undefined) {
-            const dbConnection = new database(dbSettings)
-            var result = await getPostByPartialTitle(dbConnection, query.partialTitle)
-            
-            if(!result.correct) {
-                response.statusCode = 404
-                response.end(result.error)
-            } else {
-                response.setHeader('Content-Type', 'text/plain; charset=utf-8')
-                response.setHeader('X-Content-Type-Options', 'nosniff')
-                response.setHeader('Access-Control-Allow-Origin', '*')
-                response.statusCode = 200
-                response.write(JSON.stringify(result.content))
-                response.end()
-            }
-        } 
-        else if(request.method === "GET"
-        && pathName === '/getAllPosts' ) {
-            const dbConnection = new database(dbSettings)
-            var result = await getAllPosts(dbConnection)
-            
-            if(!result.correct) {
-                response.statusCode = 404
-                response.end(result.error)
-            } else {
-                response.setHeader('Content-Type', 'text/plain; charset=utf-8')
-                response.setHeader('X-Content-Type-Options', 'nosniff')
-                response.setHeader('Access-Control-Allow-Origin', '*')
-                response.statusCode = 200
-                response.write(JSON.stringify(result.content))
-                response.end()
-            }
-        } 
-        else if(request.method === "GET"
-        && pathName === '/getAllAuthors' ) {
-            const dbConnection = new database(dbSettings)
-            var result = await getAllAuthors(dbConnection)
-            
-            if(!result.correct) {
-                response.statusCode = 404
-                response.end(result.error)
-            } else {
-                response.setHeader('Content-Type', 'text/plain; charset=utf-8')
-                response.setHeader('X-Content-Type-Options', 'nosniff')
-                response.setHeader('Access-Control-Allow-Origin', '*')
-                response.statusCode = 200
-                response.write(JSON.stringify(result.content))
-                response.end()
-            }
+    if(query.id !== undefined
+    && query.column !== undefined && columnOptions.includes(query.column)
+    && query.change !== undefined && (query.change == 1 || query.change == -1)) {
+        const dbConnection = new database(dbSettings)
+        var result = await putUpdateStatus(dbConnection, query.id,
+            query.column, query.change)
+    
+        if(!result.correct) {
+            response.status(404).end(result.error.toString())
         } else {
-            response.StatusCode = 400
-            response.end()
+            response.status(200).end()
+        }
+    } 
+})
+
+app.get('/getPostById', async(request, response) => {
+    const parsedUrl = url.parse(request.url, true)
+    const query = parsedUrl.query
+    if(query.id !== undefined) {
+        const dbConnection = new database(dbSettings)
+        var result = await getPostById(dbConnection, query.id)
+        
+        if(!result.correct) {
+            response.status(404).end(JSON.stringify(result.content))
+        } else {
+            response.set( {
+                'Content-Type': 'text/plain',
+                'Charset': 'utf-8',
+                'X-Content-Type-Options': 'nosniff',
+                'Access-Control-Allow-Origin': '*'
+            }).status(200).send(JSON.stringify(result.content))
         }
     }
-}).listen(port)
+})
 
-console.log(`Application at: ${port}`)
+app.get('/getPostByPartialTitle', async(request, response) => {
+    const parsedUrl = url.parse(request.url, true)
+    const query = parsedUrl.query
+    if(query.partialTitle !== undefined) {
+        const dbConnection = new database(dbSettings)
+        var result = await getPostByPartialTitle(dbConnection, query.partialTitle)
+        
+        if(!result.correct) {
+            response.status(404).end(result.error)
+        } else {
+            response.set( {
+                'Content-Type': 'text/plain',
+                'Charset': 'utf-8',
+                'X-Content-Type-Options': 'nosniff',
+                'Access-Control-Allow-Origin': '*'
+            }).status(200).send(JSON.stringify(result.content))
+        }
+    }
+})
+
+app.get('/getAllPosts', async (request, response) => {
+    const dbConnection = new database(dbSettings)
+    var result = await getAllPosts(dbConnection)
+    
+    if(!result.correct) {
+        response.status(404).end(result.error)
+    } else {
+        response.set({
+            'Content-Type': 'text/plain',
+            'Charset': 'utf-8',
+            'X-Content-Type-Options': 'nosniff',
+            'Access-Control-Allow-Origin': '*'
+        }).status(200).send(JSON.stringify(result.content))
+    }
+})
+
+app.get('/getAllAuthors', async (request, response) => {
+    const dbConnection = new database(dbSettings)
+    var result = await getAllAuthors(dbConnection)
+    
+    if(!result.correct) {
+        response.status(404).end(result.error)
+    } else {
+        response.set({
+            'Content-Type': 'text/plain',
+            'Charset': 'utf-8',
+            'X-Content-Type-Options': 'nosniff',
+            'Access-Control-Allow-Origin': '*'
+        }).status(200).send(JSON.stringify(result.content))
+    }
+})
+
+app.use("/favicon.ico", express.static('public/favicon.ico')); 
+
+server.listen(port, () => console.log(`Application at: ${port}`))
